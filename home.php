955b0dc534c40c7fbd6c54b1271766d64982a59d
bzee2004 <brianzee2004@gmail.com>
2025-04-29 00:31:51 -0700

    changes

--- home.php
+++ home.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sports</title>
+    <title>CSUF Volleyball Team</title>
     <link rel="stylesheet" href="./home.css">
 </head>
 <body>
-    <h1>Testing</h1>
+    <?php 
+        include_once("./head.php");
+    ?>
+    <div class="header">
+        <h1>CSUF Volleyball Team</h1>
+        <a href="./login/login.php">Log in</a>
+    </div>
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php 
+        include_once("../config.php");
+    ?>
+    <nav>
+        navgation bar
+        <a href="<?php echo $BASE_PATH ?>/login/login.php">Log in</a>
+    </nav>
+</body>
+</html>
