16d5395c7f53c6f5d9bfac59a33c51bd218e8fdf
bzee2004 <brianzee2004@gmail.com>
2025-04-29 18:30:56 -0700

    more ddl changes
    - Player and Staff now have userID linked to them

--- setup/ddl.sql
+++ setup/ddl.sql
@@ -15,6 +15,20 @@ CREATE TABLE Positions (
     positionName VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE UserRole (
+    roleID VARCHAR(10) NOT NULL PRIMARY KEY,
+    rolePriority INT(5) UNSIGNED NOT NULL
+);
+
+CREATE TABLE Account (
+    userID VARCHAR(100) NOT NULL PRIMARY KEY,
+    userPassword VARCHAR(100) NOT NULL,
+    userEmail VARCHAR(100) NOT NULL,
+    roleID VARCHAR(10) NOT NULL,
+
+    FOREIGN KEY (roleID) REFERENCES UserRole(roleID) ON DELETE CASCADE
+);
+
 CREATE TABLE HomeTeam (
     teamID INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     teamName VARCHAR(100) NOT NULL
@@ -32,9 +46,22 @@ CREATE TABLE Player (
     firstName VARCHAR(100) NOT NULL,
     lastName VARCHAR(100) NOT NULL,
     positionID VARCHAR(5) NOT NULL,
+    userID VARCHAR(100) DEFAULT NULL,
 
     FOREIGN KEY (teamID) REFERENCES HomeTeam(teamID) ON DELETE CASCADE,
-    FOREIGN KEY (positionID) REFERENCES Positions(positionID) ON DELETE CASCADE
+    FOREIGN KEY (positionID) REFERENCES Positions(positionID) ON DELETE CASCADE,
+    FOREIGN KEY (userID) REFERENCES Account(userID)
+);
+
+CREATE TABLE Staff (
+    staffID INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    teamID INT(10) UNSIGNED NOT NULL,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    userID VARCHAR(100),
+
+    FOREIGN KEY (teamID) REFERENCES HomeTeam(teamID) ON DELETE CASCADE,
+    FOREIGN KEY (userID) REFERENCES Account(userID)
 );
 
 CREATE TABLE GameStats (
@@ -86,31 +113,20 @@ CREATE TABLE PlayerStats (
     FOREIGN KEY (gameID) REFERENCES GameStats(gameID) ON DELETE CASCADE
 );
 
-CREATE TABLE Staff (
-    staffID INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    teamID INT(10) UNSIGNED NOT NULL,
-    firstName VARCHAR(100) NOT NULL,
-    lastName VARCHAR(100) NOT NULL,
-    staffRole VARCHAR(30) NOT NULL,
-
-    FOREIGN KEY (teamID) REFERENCES HomeTeam(teamID) ON DELETE CASCADE
-);
-
-CREATE TABLE UserRole (
-    roleID VARCHAR(10) NOT NULL PRIMARY KEY,
-    rolePriority INT(5) UNSIGNED NOT NULL
-);
-
-CREATE TABLE Account (
-    userID VARCHAR(100) NOT NULL PRIMARY KEY,
-    userPassword VARCHAR(100) NOT NULL,
-    userEmail VARCHAR(100) NOT NULL,
-    roleID VARCHAR(10) NOT NULL,
-
-    FOREIGN KEY (roleID) REFERENCES UserRole(roleID) ON DELETE CASCADE
-);
-
 -- Inserting values
+
+INSERT INTO UserRole VALUES
+    ("Guest", 0),
+    ("Player", 1),
+    ("Coach", 2),
+    ("Manager", 3);
+
+INSERT INTO Account (userID, userPassword, userEmail, roleID) VALUES
+    ("testPlayer", "iAmPlayer!", "brzee@csu.fullerton.edu", "Player"),
+    ("testCoach", "iAmCoach!", "brzee@csu.fullerton.edu", "Coach"),
+    ("testManager", "iAmManager!", 'brzee@csu,fullerton.edu', "Manager"),
+
+    ("terry_jones", "iAmTerry!", "terryjones@gmail.com", "Player");
 
 INSERT INTO HomeTeam (teamName) VALUES
     ("Cal State Slammers");
@@ -133,18 +149,22 @@ INSERT INTO Positions VALUES
     ("DS", "Defensive Specialist"),
     ("SS", "Serving Specialist");
 
-INSERT INTO Player (teamID, firstName, lastName, positionID) VALUES
-    (1, "Terry", "Jones", "L"),
-    (1, "Volley", "Ball", "S"),
-    (1, "Mr", "Setterman", "S"),
-    (1, "Balley", "Voll", "OH"),
-    (1, "Hitter", "Guy", "OH"),
-    (1, "Bob", "Baller", "OP"),
-    (1, "Benjamin", "Ballman", "OP"),
-    (1, "Jordan", "Mikael", "MB"),
-    (1, "Big", "Strongman", "MB"),
-    (1, "Marcus", "Lee", "DS"),
-    (1, "Noah", "Thompson", "SS");
+INSERT INTO Player (teamID, firstName, lastName, positionID, userID) VALUES
+    (1, "Terry", "Jones", "L", "terry_jones"),
+    (1, "Volley", "Ball", "S", DEFAULT),
+    (1, "Mr", "Setterman", "S", DEFAULT),
+    (1, "Balley", "Voll", "OH", DEFAULT),
+    (1, "Hitter", "Guy", "OH", DEFAULT),
+    (1, "Bob", "Baller", "OP", DEFAULT),
+    (1, "Benjamin", "Ballman", "OP", DEFAULT),
+    (1, "Jordan", "Mikael", "MB", DEFAULT),
+    (1, "Big", "Strongman", "MB", DEFAULT),
+    (1, "Marcus", "Lee", "DS", DEFAULT),
+    (1, "Noah", "Thompson", "SS", DEFAULT);
+
+INSERT INTO Staff (teamID, firstName, lastName, userID) VALUES
+    (1, "Mister", "Manager", "testManager"),
+    (1, "Coach", "Coachington", "testCoach");
 
 INSERT INTO GameStats (teamID, opponentID, gameDate, result) VALUES
     (1, 1, '2024-11-15', "win"),
@@ -163,18 +183,3 @@ INSERT INTO SetStats VALUES
 INSERT INTO PlayerStats VALUES
     (1, 1, 12, 18, 0.45, 0.65, 0.85, 0.78),
     (2, 1, 15, 7, 0.55, 0.51, 0.23, 0.67);
-
-INSERT INTO UserRole VALUES
-    ("Guest", 0),
-    ("Player", 1),
-    ("Coach", 2),
-    ("Manager", 3);
-
-INSERT INTO Account (userID, userPassword, userEmail, roleID) VALUES
-    ("player", "iAmPlayer!", "brzee@csu.fullerton.edu", "Player"),
-    ("coach", "iAmCoach!", "brzee@csu.fullerton.edu", "Coach"),
-    ("manager", "iAmManager!", 'brzee@csu,fullerton.edu', "Manager");
-
-INSERT INTO Staff (teamID, firstName, lastName, staffRole) VALUES
-    (1, "Mister", "Manager", "manager"),
-    (1, "Coach", "Coachington", "coach");
